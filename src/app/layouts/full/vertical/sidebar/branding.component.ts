513206c9f5e3a16abb4887d3d25f876299a58cf5
hHanan13 <34395039+hHanan13@users.noreply.github.com>
2024-10-09 17:51:26 +0300

    update

---
+++ src/app/layouts/full/vertical/sidebar/branding.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CoreService } from 'src/app/services/core.service';
+
+@Component({
+  selector: 'app-branding',
+  standalone: true,
+  imports: [RouterModule],
+  template: `
+    <div class="branding">
+      @if(options.theme === 'light') {
+      <a href="/">
+        Electron
+      </a>
+      } @if(options.theme === 'dark') {
+      <a href="/">
+        <img
+          src="./assets/images/logos/light-logo.svg"
+          class="align-middle m-2"
+          alt="logo"
+        />
+      </a>
+      }
+    </div>
+  `,
+})
+export class BrandingComponent {
+  options = this.settings.getOptions();
+
+  constructor(private settings: CoreService) { }
+}
